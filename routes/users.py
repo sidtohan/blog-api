985469dbf9f53d30c28e3de8801c0c50a507ea5c
Siddhant Tohan <siddhant.tohan@gmail.com>
2024-01-30 22:43:22 +0530

    New: fixed login bug on the open api documentation

--- routes/users.py
+++ routes/users.py
@@ -1,8 +1,10 @@
 # Lib
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends, Form
+from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 from datetime import timezone, timedelta, datetime
 from jose import jwt, JWTError
+from typing import Annotated
 
 # Models
 from models.user import User, UserLR, Token, TokenData
@@ -14,10 +16,11 @@ user_router = APIRouter(prefix='/users', tags=['users'])
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated="auto")
 
 
-# For JWT
+# For Auth
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="users/login")
 
 # Utility Functions
 def verify_password(plain_pass: str, hashed_pass: str) -> bool:
@@ -58,7 +61,7 @@ async def get_active_user(token: str) -> User | None:
         token_data = TokenData(username=username)
     except JWTError:
         raise credentials_exception
-    user = get_user(username=token_data.username)
+    user = await get_user(username=token_data.username)
     if user is None:
         raise credentials_exception
     return user
@@ -73,17 +76,16 @@ async def register_user(user: User):
     # Add
     try:
         user.password = get_password_hash(user.password)
-        new_user = conn.blogApp.users.insert_one(dict(user))
+        new_user = conn.blogAPI.users.insert_one(dict(user))
         return {"Message": "User added successfully"}
     except Exception as e:
         print("Error occurred:", e)
 
 # Login Endpoint
 @user_router.post("/login")
-async def login_user(user: UserLR):
-    username = user.username
-    password = user.password
-    find_user = conn.blogApp.users.find_one({"username": username})
+async def login_user(username: Annotated[str, Form()], password: Annotated[str, Form()]):
+    print(username)
+    find_user = conn.blogAPI.users.find_one({"username": username})
     if find_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     hashed_password = find_user["password"]
@@ -91,6 +93,24 @@ async def login_user(user: UserLR):
         raise HTTPException(status_code=401, detail="Incorrect password")
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
+        data={"sub": username}, expires_delta=access_token_expires
     )
     return Token(access_token=access_token, token_type="bearer")
+
+# Update Endpoint
+@user_router.put("/update")
+async def update_user(user: User, token: Annotated[str, Depends(oauth2_scheme)]):
+    username = user.username
+    password = user.password
+    find_user = conn.blogAPI.users.find_one({"username": username})
+    if find_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    # Updation possible only if user is logged in
+    token_user = await get_active_user(token)
+    if token_user["username"] != username or verify_password(password, token_user["password"]) == False:
+        raise HTTPException(status_code=401)
+
+    # Update user now
+    user.password = get_password_hash(user.password)
+    conn.blogAPI.users.find_one_and_update({"_id": find_user["_id"]}, {"$set": dict(user)})
+    return {"Message": "User updated successfully"}
