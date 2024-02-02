1c6174a702a4db8e3b403bca3fde6a97c896b038
Siddhant Tohan <siddhant.tohan@gmail.com>
2024-02-02 19:19:40 +0530

    New: fixed bug in dotenv

--- config/db.py
+++ config/db.py
@@ -1,10 +1,12 @@
 # Dotenv
-from dotenv import dotenv_values
+from os import getenv
+from dotenv import load_dotenv
 
 # Pymongo
 from pymongo import MongoClient
-config = dotenv_values(".env")
-MONGO_URI = config["MONGODB_URI"]
+
+load_dotenv()
+MONGO_URI = getenv("MONGODB_URI")
 
 conn = MongoClient(MONGO_URI)
 
